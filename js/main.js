f2ae0097fd924c1cde694a6786641a706959f580
Marissa <marissa.biesecker@gmail.com>
2018-09-20 23:46:10 +0100

    Add meta tags to index.html, api keys to js files

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<YOUR_MAPBOX_ACCESS_TOKEN>',
+    mapboxToken: 'pk.eyJ1IjoibWFyaWJpZXMiLCJhIjoiY2ptYjVzN3I1MTY0ZDNwbm9wNWw5MXk4eCJ9.hACLatXlqKPLnleQ_zxTjw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
